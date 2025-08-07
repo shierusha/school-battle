f94111f2ea6691821a3ecabb04c4953121546742
Roro <tinacat1518@gmail.com>
2025-08-08 00:26:51 +0800

    Update get-st.js

--- students/get-st.js
+++ students/get-st.js
@@ -10,6 +10,7 @@ const TEST_STUDENT_IDS = [
   '845dd04d-ff39-43a8-ad84-e8f714a8b454', //工具寵
   'fe4e4c3e-66ae-4ded-afe6-417f73979131', //梨
   '7712a95b-7884-4e34-8dba-65ff01084b16', //咕咕
+  '7edd83a7-3d44-4876-a480-a8bc795f48b3', //碎影
   'ee8ea0cd-d664-4b5f-8d0e-0240bf9debdb'//現荔枝
 ];
 const SPECIAL_PLAYER_ID = '8aea3076-294c-41f6-bb38-0a99da77c098'; //其實是AI
