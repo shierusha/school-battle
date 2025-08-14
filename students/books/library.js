82fdfabfdc275ccccffc0c1a33534eadffce1bda
Roro <tinacat1518@gmail.com>
2025-08-15 02:36:10 +0900

    Update library.js

--- students/books/library.js
+++ students/books/library.js
@@ -71,7 +71,7 @@
     const {h,l}=hexToHsl(hex);
     const hue = Math.round(h);
     const bright = Math.round(60 + (l*40)); // 60%~100%
-    return `sepia(0.4) saturate(300%) hue-rotate(${hue}deg) brightness(${bright}%)`;
+    return `sepia(0.1) saturate(320%) hue-rotate(${hue}deg) brightness(${bright}%)`;
   }
 
   // ---------------- DOM refs ----------------
